1fe57339b5b036548e83e94da40e067b0743e8b4
Jorge Altamirano <alojorge13@gmail.com>
2026-04-16 16:06:25 -0300

    modificando packege.json

--- src/server.ts
+++ src/server.ts
@@ -19,6 +19,7 @@ app.use("/api/public", PublicRouter);
 app.use("/api/servicios", ServiciosRouter);
 app.use("/api/horarios", HorariosRouter);
 
-app.listen(3000, () => {
-  console.log("🚀 Server corriendo en puerto 3000");
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log("🚀 Server corriendo en puerto", PORT);
 });
