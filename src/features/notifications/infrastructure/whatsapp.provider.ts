30410ed3dae8edf878471ad3353c52e0cf4f8231
Jorge Altamirano <alojorge13@gmail.com>
2026-04-27 12:27:49 -0300

    modificando el sendwhatsapp

--- src/features/notifications/infrastructure/whatsapp.provider.ts
+++ src/features/notifications/infrastructure/whatsapp.provider.ts
@@ -3,14 +3,19 @@ import { twilioClient } from "./twilio.client";
 const FROM = process.env.TWILIO_WHATSAPP_NUMBER!;
 
 export const sendWhatsApp = async (to: string, body: string) => {
-  try {
-    await twilioClient.messages.create({
-      from: FROM,
-      to: `whatsapp:${to}`,
-      body,
-    });
-  } catch (error) {
-    console.error("Error enviando WhatsApp:", error);
-    throw error;
-  }
+    try {
+        console.log("ACCOUNT SID:", process.env.TWILIO_ACCOUNT_SID);
+        console.log("FROM:", process.env.TWILIO_WHATSAPP_NUMBER);
+        console.log("TO:", to);
+       const message = await twilioClient.messages.create({
+            from: FROM,
+            to: `whatsapp:${to}`,
+            body,
+        });
+        console.log("SID:", message.sid);
+console.log("STATUS:", message.status);
+    } catch (error) {
+        console.error("Error enviando WhatsApp:", error);
+        throw error;
+    }
 };
