21ca259c0a2b9c698d73183e1ed022e3538de9d8
Jorge Altamirano <alojorge13@gmail.com>
2026-05-20 19:40:10 -0300

    agregando console

--- src/config/db.ts
+++ src/config/db.ts
@@ -2,6 +2,13 @@ import mysql from "mysql2/promise"
 import dotenv from 'dotenv'
 dotenv.config();
 
+// 👇 AGREGÁ ESTO
+console.log("DB_HOST:", process.env.DB_HOST)
+console.log("DB_PORT:", process.env.DB_PORT)
+console.log("DB_USER:", process.env.DB_USER)
+console.log("DB_NAME:", process.env.DB_NAME)
+console.log("DATABASE_URL:", process.env.DATABASE_URL)
+
 export const pool = mysql.createPool({
     host: process.env.DB_HOST,
     port: Number(process.env.DB_PORT),
