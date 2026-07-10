c021a2f2893ffefb7acd99f908b5da33af4090d7
Jorge Altamirano <alojorge13@gmail.com>
2026-07-10 15:48:04 -0300

    agregando tiempo

--- src/features/notifications/infrastructure/evolution.client.ts
+++ src/features/notifications/infrastructure/evolution.client.ts
@@ -6,7 +6,7 @@ const EVOLUTION_KEY = process.env.EVOLUTION_API_KEY!;
 
 export const evolutionClient = axios.create({
   baseURL: EVOLUTION_URL,
-   timeout: 5000,
+   timeout: 15000,
   headers: {
     apikey: EVOLUTION_KEY,
     "Content-Type": "application/json",
